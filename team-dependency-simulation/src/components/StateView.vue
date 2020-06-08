8ee25920429ead0f2d9d6c956d30042a2367556b
jjleight <jjleight@yahoo.co.uk>
2020-06-08 08:42:09 +0100

    [JL] more styling tweaks

--- team-dependency-simulation/src/components/StateView.vue
+++ team-dependency-simulation/src/components/StateView.vue
@@ -1,56 +1,62 @@
 <template>
-  <div class="state mb-5">
-    <h2>
-      State <span v-if="state['runType']">({{ state["runType"] }})</span
-      ><span v-if="state['strategy']">, Strategy {{ strategy }}</span>
-    </h2>
-    <div class="narration">
-      <div v-if="state['narration'].length"><strong>All Blocked: </strong></div>
-      <div
-        v-for="item in state['narration']"
-        :key="item['card']['suit'] + '-' + item['card']['card']"
-      >
-        <div class="suit-label-small" :class="item['suit']"></div>
-        <span> is playing </span>
-        <span>{{ getCard(item["card"]["card"]) }} of </span>
-        <div class="suit-label-small" :class="item['card']['suit']"></div>
-        <span>. </span>
+  <div class="state mb-5 row text-center">
+    <div class="col-md-12">
+      <h2>
+        State <span v-if="state['runType']">({{ state["runType"] }})</span
+        ><span v-if="state['strategy']">, Strategy {{ strategy }}</span>
+      </h2>
+      <div class="narration">
+        <div v-if="state['narration'].length">
+          <strong>All Blocked: </strong>
+        </div>
+        <div
+          v-for="item in state['narration']"
+          :key="item['card']['suit'] + '-' + item['card']['card']"
+        >
+          <div class="suit-label-small" :class="item['suit']"></div>
+          <span> is playing </span>
+          <span>{{ getCard(item["card"]["card"]) }} of </span>
+          <div class="suit-label-small" :class="item['card']['suit']"></div>
+          <span>. </span>
+        </div>
       </div>
     </div>
-    <div class="suit">
-      <div v-for="(cards, suit) in state['suits']" :key="suit" :class="suit">
-        <div class="suit-label" :class="suit"></div>
-        <div class="label current">Current:</div>
-        <div
-          :class="[suit, getCard(state['suits'][suit]['current'])]"
-          class="card"
-        ></div>
-        <div class="label blocked">Blocked:</div>
-        <div v-if="!state['suits'][suit]['blocked']" class="blocked">No</div>
-        <div v-if="state['suits'][suit]['blocked']" class="blocked">Yes</div>
-        <div class="label">Own:</div>
-        <div class="own">
-          <div v-for="card in suit_cards" :key="card">
-            <div
-              v-if="!missing(card, state['suits'][suit]['cards'])"
-              :class="[card, suit]"
-              class="card"
-            ></div>
-            <div
-              v-if="missing(card, state['suits'][suit]['cards'])"
-              class="card"
-              :style="{ 'background-color': cardDone(suit, card) }"
-            ></div>
-          </div>
-        </div>
-        <div class="label">Others:</div>
-        <div class="others">
+    <div class="suit row">
+      <div class="col-md-12">
+        <div v-for="(cards, suit) in state['suits']" :key="suit" :class="suit">
+          <div class="suit-label" :class="suit"></div>
+          <div class="label current">Current:</div>
           <div
-            v-for="card in state['suits'][suit]['others']"
-            :key="card['suit'] + '-' + card['card']"
-            :class="[card['suit'], getCard(card['card'])]"
+            :class="[suit, getCard(state['suits'][suit]['current'])]"
             class="card"
           ></div>
+          <div class="label blocked">Blocked:</div>
+          <div v-if="!state['suits'][suit]['blocked']" class="blocked">No</div>
+          <div v-if="state['suits'][suit]['blocked']" class="blocked">Yes</div>
+          <div class="label">Own:</div>
+          <div class="own">
+            <div v-for="card in suit_cards" :key="card">
+              <div
+                v-if="!missing(card, state['suits'][suit]['cards'])"
+                :class="[card, suit]"
+                class="card"
+              ></div>
+              <div
+                v-if="missing(card, state['suits'][suit]['cards'])"
+                class="card"
+                :style="{ 'background-color': cardDone(suit, card) }"
+              ></div>
+            </div>
+          </div>
+          <div class="label">Others:</div>
+          <div class="others">
+            <div
+              v-for="card in state['suits'][suit]['others']"
+              :key="card['suit'] + '-' + card['card']"
+              :class="[card['suit'], getCard(card['card'])]"
+              class="card"
+            ></div>
+          </div>
         </div>
       </div>
     </div>
