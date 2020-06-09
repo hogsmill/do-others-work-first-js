7e6192fcca8e686293f839f8bc1434e874585ef1
hogsmill <info@hogsmill.com>
2020-06-09 13:42:32 +0100

    no of sockets validation

--- team-dependency-simulation/src/server.js
+++ team-dependency-simulation/src/server.js
@@ -1,13 +1,15 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-
 const os = require('os')
 
 var prod = os.hostname() == "agilesimulations" ? true : false
 
 var connectDebugOff = prod
 var debugOn = !prod
+
+var connections = 0
+var maxConnections = 10
 
 function emit(event, data) {
   if (debugOn) {
@@ -17,9 +19,16 @@ function emit(event, data) {
 }
 
 io.on("connection", (socket) => {
-  connectDebugOff || console.log(`A user connected with socket id ${socket.id}.`)
+  connections = connections + 1
+    if (connections > maxConnections) {
+      console.log(`Too many connections. Socket ${socket.id} closed`)
+      socket.disconnect(0)
+    } else {
+      connectDebugOff || console.log(`A user connected with socket id ${socket.id}. (${connections} connections)`)
+    }
 
   socket.on("disconnect", () => {
+    connections = connections - 1
     connectDebugOff || console.log(`User with socket id ${socket.id} has disconnected.`)
   })
 
